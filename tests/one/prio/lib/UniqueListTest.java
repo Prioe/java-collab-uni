878e462938609a8c4dd43b12dd255415b01ce87e
Michael Albertz <michael@prio.one>
2017-11-02 11:55:40 +0100

    updated test cases to another convention

--- tests/one/prio/lib/UniqueListTest.java
+++ tests/one/prio/lib/UniqueListTest.java
@@ -7,6 +7,7 @@
 
 package one.prio.lib;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -15,111 +16,126 @@ import java.util.Collections;
 import static org.junit.Assert.*;
 
 public class UniqueListTest {
+  private UniqueList ul;
+
+  @Before
+  public void initialize() {
+    ul = new UniqueList(1, 2, 3);
+  }
 
   @Test
-  public void constructor() throws Exception {
+  public void testConstructor_passingNoArguments_shouldCreateAnEmptyList() throws Exception {
     UniqueList ul0 = new UniqueList();
-    assertArrayEquals(ul0.toArray(), new Integer[0]);
-    UniqueList ul1 = new UniqueList(1, 2, 3);
-    assertArrayEquals(ul1.toArray(), new Integer[]{1, 2, 3});
-    UniqueList ul2 = new UniqueList(1, 2, 1);
-    assertArrayEquals(ul2.toArray(), new Integer[]{1, 2});
+    assertArrayEquals(new Integer[]{}, ul0.toArray());
   }
 
   @Test
-  public void add() throws Exception {
-    UniqueList ul1 = new UniqueList(1, 2, 3);
-    assertTrue(ul1.add(4));
-    assertArrayEquals(ul1.toArray(), new Integer[]{1, 2, 3, 4});
-
-    UniqueList ul2 = new UniqueList(1, 2, 3);
-    assertFalse(ul2.add(2));
-    assertArrayEquals(ul2.toArray(), new Integer[]{1, 2, 3});
+  public void testConstructor_passingAUniqueArray_shouldDuplicateTheArray() throws Exception {
+    UniqueList ul0 = new UniqueList(1, 2, 3);
+    assertArrayEquals(new Integer[]{1, 2, 3}, ul0.toArray());
   }
 
   @Test
-  public void add1() throws Exception {
-    UniqueList ul1 = new UniqueList(1, 2, 3);
-    ul1.add(0, 4);
-    assertArrayEquals(ul1.toArray(), new Integer[]{4, 1, 2, 3});
+  public void testConstructor_passingANonUniqueArray_shouldRemoveTheDuplicates() throws Exception {
+    UniqueList ul0 = new UniqueList(1, 2, 1);
+    assertArrayEquals(new Integer[]{1, 2}, ul0.toArray());
+  }
 
-    UniqueList ul2 = new UniqueList(1, 2, 3);
-    ul2.add(3, 4);
-    assertArrayEquals(ul2.toArray(), new Integer[]{1, 2, 3, 4});
+  @Test
+  public void testAdd_addingNonExistentElement_shouldAppendElement() throws Exception {
+    assertTrue(ul.add(4));
+    assertArrayEquals(new Integer[]{1, 2, 3, 4}, ul.toArray());
+  }
 
+  @Test
+  public void testAdd_addingDuplicate_shouldNotAlterList() throws Exception {
+    assertFalse(ul.add(2));
+    assertArrayEquals(new Integer[]{1, 2, 3}, ul.toArray());
+  }
+
+  @Test
+  public void testAdd_addingAUniqueElementAtAValidIndex_shouldInsertTheElement() throws Exception {
+    ul.add(0, 4);
+    assertArrayEquals(new Integer[]{4, 1, 2, 3}, ul.toArray());
+
+  }
+
+  @Test
+  public void testAdd_addingAUniqueElementAtListLength_shouldAppendTheElement() throws Exception {
+    ul.add(3, 4);
+    assertArrayEquals(new Integer[]{1, 2, 3, 4}, ul.toArray());
   }
 
   @Test(expected = IndexOutOfBoundsException.class)
-  public void add1OutOfBounds() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testAdd_addingAtAnInvalidIndex_shouldThrowOutOfBounds() throws Exception {
     ul.add(10, 4);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void add1IllegalArgument() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testAdd_addingADuplicateElement_shouldThrowIllegalArgument() throws Exception {
     ul.add(0, 3);
   }
 
   @Test
-  public void addAll() throws Exception {
-    UniqueList ul1 = new UniqueList(1, 2, 3);
-    assertTrue(ul1.addAll(Arrays.asList(4, 5, 6)));
-    assertArrayEquals(ul1.toArray(), new Integer[]{1, 2, 3, 4, 5, 6});
-
-    UniqueList ul3 = new UniqueList(1, 2, 3);
-    assertTrue(ul3.addAll(Arrays.asList(2, 3, 4)));
-    assertArrayEquals(ul3.toArray(), new Integer[]{1, 2, 3, 4});
-
-    UniqueList ul2 = new UniqueList(1, 2, 3);
-    assertFalse(ul2.addAll(Arrays.asList(1, 2, 3)));
-    assertArrayEquals(ul2.toArray(), new Integer[]{1, 2, 3});
+  public void testAddAll_addingUniqueElements_shouldAppendElements() throws Exception {
+    assertTrue(ul.addAll(Arrays.asList(4, 5, 6)));
+    assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, ul.toArray());
   }
 
   @Test
-  public void addAll1() throws Exception {
-    UniqueList ul1 = new UniqueList(1, 2, 3);
-    assertTrue(ul1.addAll(1, Arrays.asList(4, 5, 6)));
-    assertArrayEquals(ul1.toArray(), new Integer[]{1, 4, 5, 6, 2, 3});
+  public void testAddAll_addingDuplicateElements_shouldOmitTheDuplicates() throws Exception {
+    assertTrue(ul.addAll(Arrays.asList(2, 3, 4)));
+    assertArrayEquals(new Integer[]{1, 2, 3, 4}, ul.toArray());
+  }
 
-    UniqueList ul2 = new UniqueList(1, 2, 3);
-    assertFalse(ul2.addAll(1, Collections.emptyList()));
-    assertArrayEquals(ul2.toArray(), new Integer[]{1, 2, 3});
+  @Test
+  public void testAddAll_addingOnlyDuplicateElements_shouldNotMutateTheList() throws Exception {
+    assertFalse(ul.addAll(Arrays.asList(1, 2, 3)));
+    assertArrayEquals(new Integer[]{1, 2, 3}, ul.toArray());
+  }
+
+  @Test
+  public void testAddAll_addingUniqueElementsAtAValidIndex_shouldInsertTheElements() throws Exception {
+    assertTrue(ul.addAll(1, Arrays.asList(4, 5, 6)));
+    assertArrayEquals(new Integer[]{1, 4, 5, 6, 2, 3}, ul.toArray());
+  }
+
+  @Test
+  public void testAddAll_addingAnEmptyList_shouldNotMutateTheList() throws Exception {
+    assertFalse(ul.addAll(1, Collections.emptyList()));
+    assertArrayEquals(new Integer[]{1, 2, 3}, ul.toArray());
   }
 
   @Test(expected = IndexOutOfBoundsException.class)
-  public void addAll1OutOfBounds() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testAddAll_addingElementsAtAnInvalidIndex_shouldThrowOutOfBounds() throws Exception {
     ul.addAll(-1, Arrays.asList(4, 5, 6));
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void addAll1IllegalArgument() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testAddAll_addingDuplicateElementsAtAValidIndex_shouldThrowIllegalArgument() throws Exception {
     ul.addAll(1, Arrays.asList(2, 3, 4));
   }
 
 
   @Test
-  public void set() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testSet_settingAnElementAtAValidIndex_shouldReplaceTheElementAtTheIndex() throws Exception {
     assertEquals((int) ul.set(0, 4), 1);
     assertArrayEquals(ul.toArray(), new Integer[]{4, 2, 3});
+  }
 
-    ul = new UniqueList(1, 2, 3);
+  @Test
+  public void testSet_settingAnDuplicateElementAtAValidIndex_shouldNotMutateTheList() throws Exception {
     assertEquals((int) ul.set(0, 1), 1);
     assertArrayEquals(ul.toArray(), new Integer[]{1, 2, 3});
   }
 
   @Test(expected = IndexOutOfBoundsException.class)
-  public void setOutOfBounds() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testSet_settingAnElementsAtAnInvalidIndex_shouldThrowOutOfBounds() throws Exception {
     ul.set(3, 4);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void setIllegalArgument() throws Exception {
-    UniqueList ul = new UniqueList(1, 2, 3);
+  public void testSet_settingADuplicateElementsAtAValidIndex_shouldThrowIllegalArgument() throws Exception {
     ul.set(1, 1);
   }
 
---
+++ tests/one/prio/lib/bag/TreeBagTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017-present, Michael Albertz, Thimo Schneider.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package one.prio.lib.bag;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TreeBagTest {
+  @Test
+  public void toString() throws Exception {
+  }
+
+  @Test
+  public void add() throws Exception {
+  }
+
+  @Test
+  public void add1() throws Exception {
+  }
+
+  @Test
+  public void contains() throws Exception {
+  }
+
+  @Test
+  public void iterator() throws Exception {
+  }
+
+  @Test
+  public void remove() throws Exception {
+  }
+
+  @Test
+  public void removeAll() throws Exception {
+  }
+
+  @Test
+  public void size() throws Exception {
+  }
+
+  @Test
+  public void clear() throws Exception {
+  }
+
+}

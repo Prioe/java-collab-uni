f14882be5d5934c7b496c57d4ed11c6ef5d4c2d3
Michael Albertz <michael@prio.one>
2017-11-02 12:11:47 +0100

    added javadoc

--- src/one/prio/lib/UniqueList.java
+++ src/one/prio/lib/UniqueList.java
@@ -14,11 +14,24 @@ import java.util.Objects;
 
 public class UniqueList extends ArrayList<Integer> {
 
-  public UniqueList(Integer... args) {
+  /**
+   * Erhält eine beliebige Anzahl ganzer Zahlen und initialisiert die Liste damit. Ignoriert Duplikate, das heißt
+   * Elemente, die schon weiter vorne vorgekommen sind.
+   *
+   * @param elements Elemente.
+   */
+  public UniqueList(Integer... elements) {
     super();
-    this.addAll(Arrays.asList(args));
+    this.addAll(Arrays.asList(elements));
   }
 
+  /**
+   * Erhält ein Element und fügt es hinten an. Ignoriert Duplikate. Liefert true, wenn diese Liste wächst und false
+   * ansonsten.
+   *
+   * @param n Neues Element.
+   * @return wurde Element hinzugefügt.
+   */
   @Override
   public boolean add(Integer n) {
     Objects.requireNonNull(n);
@@ -26,6 +39,15 @@ public class UniqueList extends ArrayList<Integer> {
     return !elementExist && super.add(n);
   }
 
+  /**
+   * Erhält einen Index und ein Element und schiebt das Element am Index ein. Die vorhandenen Elemente machen Platz
+   * und rücken nach hinten. Liefert nichts
+   *
+   * @param idx Index an dem das Element hinzugefügt wird.
+   * @param n   Neues Element.
+   * @throws IndexOutOfBoundsException wenn der gewählte Index zu hoch oder zu niedrig ist.
+   * @throws IllegalArgumentException  wenn das Element schon vorhanden ist.
+   */
   @Override
   public void add(int idx, Integer n) throws IndexOutOfBoundsException, IllegalArgumentException {
     Objects.requireNonNull(n);
@@ -36,6 +58,14 @@ public class UniqueList extends ArrayList<Integer> {
     super.add(idx, n);
   }
 
+  /**
+   * Erhält eine Collection mit ganzen Zahlen und fügt deren Elemente alle nacheinander hinten an diese Liste an.
+   * Lässt Duplikate weg. Liefert true, wenn diese
+   * Liste wächst und false ansonsten.
+   *
+   * @param elements Elemente.
+   * @return hat sich die Länge der Liste geändert.
+   */
   @Override
   public boolean addAll(Collection<? extends Integer> elements) {
     Objects.requireNonNull(elements);
@@ -47,6 +77,15 @@ public class UniqueList extends ArrayList<Integer> {
     return initialLength < super.size();
   }
 
+  /**
+   * Erhält einen Index und eine Collection mit ganzen Zahlen und fügt deren Elemente am Index ein. Die vorhandenen
+   * Elemente machen Platz und rücken nach
+   * hinten.
+   *
+   * @param idx Index an dem die Elemente hinzugefügt werden.
+   * @param elements Elemente.
+   * @return hat sich die Länge der Liste geändert.
+   */
   public boolean addAll(int idx, Collection<? extends Integer> elements) {
     Objects.requireNonNull(elements);
 
@@ -60,8 +99,15 @@ public class UniqueList extends ArrayList<Integer> {
     return initialLength < super.size();
   }
 
+  /**
+   * Erhält einen Index und ein Element und ersetzt das Element am Index   * @param idx
+   * @param element Element
+   * @return das ursprüngliche Element am Index.
+   * @throws IndexOutOfBoundsException wenn der gewählte Index zu hoch oder zu niedrig ist.
+   * @throws IllegalArgumentException  wenn das Element schon vorhanden ist.
+   */
   @Override
-  public Integer set(int idx, Integer element) throws IllegalArgumentException {
+  public Integer set(int idx, Integer element) throws IllegalArgumentException, IndexOutOfBoundsException {
     Objects.requireNonNull(element);
     int elementIdx = super.indexOf(element);
 
